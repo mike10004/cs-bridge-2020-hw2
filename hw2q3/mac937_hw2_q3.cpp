ac1f576b50a3f1720671e4041b0697d9a5894b71
Mike Chaberski <mchaberski@gmail.com>
2020-01-18 19:08:04 -0500

    support elementary method of addition using env var
    set environment variable ELEMENTARY=1 to use the elementary method of addition

--- hw2q3/mac937_hw2_q3.cpp
+++ hw2q3/mac937_hw2_q3.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include <climits>
 
 const int UNITS = 3;
 const char* UNIT_NAMES[] = {"days", "hours", "minutes"};
 const int MINUTES_PER_UNIT[] = {24 * 60, 60, 1};
+const int UNIT_DIVISORS[] = {INT_MAX, 24, 60};
 const int INDEX_DAYS = 0;
 const int INDEX_HOURS = 1;
 const int INDEX_MINUTES = 2;
@@ -13,7 +15,7 @@ const int INDEX_MINUTES = 2;
  * @param duration an array of length UNITS
  * @return total minutes in the duraiton
  */
-int calculate_minutes(int* duration)
+int calculate_minutes(const int* duration)
 {
     int total = 0;
     for (int i = 0; i < UNITS; i++)
@@ -21,21 +23,6 @@ int calculate_minutes(int* duration)
         total += (MINUTES_PER_UNIT[i] * duration[i]);
     }
     return total;
-}
-
-/**
- * Read a duration from standard input.
- * @param person name of the person
- * @param duration array of length UNITS
- */
-void read_duration(const char* person, int* duration)
-{
-    for (int i = 0; i < UNITS; i++)
-    {
-        std::cout << "Please enter the number of " << UNIT_NAMES[i] << " " << person << " has worked: ";
-        std::cin >> duration[i];
-    }
-    std::cout << std::endl;
 }
 
 void minutes_to_duration(int minutes, int* duration)
@@ -47,15 +34,55 @@ void minutes_to_duration(int minutes, int* duration)
     }
 }
 
+/**
+ * Sums two durations. All argument arrays must be of length UNITS.
+ * @param d1 array representing a duration
+ * @param d2 array summand representing a duration
+ * @param sum destination array 
+ */
+void add_durations(const int* d1, const int* d2, int* sum)
+{
+    if (getenv("ELEMENTARY") != nullptr && atoi(getenv("ELEMENTARY")) == 1) 
+    {
+        int carried = 0;
+        for (int i = UNITS - 1; i >= 0; i--) {
+            int unit_sum = d1[i] + d2[i] + carried;
+            int remainder = unit_sum % UNIT_DIVISORS[i];
+            sum[i] = remainder;
+            carried = unit_sum / UNIT_DIVISORS[i];
+        }
+    } else {
+        int minutes1 = calculate_minutes(d1);
+        int minutes2 = calculate_minutes(d2);
+        minutes_to_duration(minutes1 + minutes2, sum);
+    }
+}
+
+/**
+ * Read a duration from standard input.
+ * @param person name of the person
+ * @param duration destination array of length UNITS
+ */
+void read_duration(const char* person, int* duration)
+{
+    for (int i = 0; i < UNITS; i++)
+    {
+        std::cout << "Please enter the number of " << UNIT_NAMES[i] << " " << person << " has worked: ";
+        std::cin >> duration[i];
+    }
+    std::cout << std::endl;
+}
+
 int main() {
     int duration_john[UNITS];
     int duration_bill[UNITS];
     read_duration("John", duration_john);
     read_duration("Bill", duration_bill);
-    int minutes_john = calculate_minutes(duration_john);
-    int minutes_bill = calculate_minutes(duration_bill);
+//    int minutes_john = calculate_minutes(duration_john);
+//    int minutes_bill = calculate_minutes(duration_bill);
     int duration_total[UNITS];
-    minutes_to_duration(minutes_john + minutes_bill, duration_total);
+//    minutes_to_duration(minutes_john + minutes_bill, duration_total);
+    add_durations(duration_john, duration_bill, duration_total);
     std::cout << "The total time both of them worked together is: "
               << duration_total[INDEX_DAYS] << " days, "
               << duration_total[INDEX_HOURS] << " hours and "
