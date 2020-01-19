52fa9092e446139b71b38722643b2b2b1ea463e3
Mike Chaberski <mchaberski@gmail.com>
2020-01-19 13:18:14 -0500

    q2: use namespace std

--- hw2q2/mac937_hw2_q2.cpp
+++ hw2q2/mac937_hw2_q2.cpp
@@ -1,27 +1,29 @@
 #include <iostream>
 
+using namespace std;
+
 const int DENOMINATIONS[] = {25, 10, 5, 1};
 const char* DENOMINATION_NAMES[] = {"quarters", "dimes", "nickels", "pennies"};
 const int DENOMINATIONS_COUNT = 4;
 
 int main() {
-    std::cout << "Please enter your amount in the format of dollars and cents separated by a space:\n";
+    cout << "Please enter your amount in the format of dollars and cents separated by a space:\n";
     int n_dollars, n_cents, total_cents;
-    std::cin >> n_dollars >> n_cents;
-    std::cout << n_dollars << " dollars and " << n_cents << " cents are:\n";
+    cin >> n_dollars >> n_cents;
+    cout << n_dollars << " dollars and " << n_cents << " cents are:\n";
     total_cents = (n_dollars * 100) + n_cents;
     for (int i = 0; i < DENOMINATIONS_COUNT; i++)
     {
         int n_coins = total_cents / DENOMINATIONS[i];
         if (i == (DENOMINATIONS_COUNT - 1))
         {
-            std::cout << " and ";
+            cout << " and ";
         }
         else if (i > 0)
         {
-            std::cout << ", ";
+            cout << ", ";
         }
-        std::cout << n_coins << " " << DENOMINATION_NAMES[i];
+        cout << n_coins << " " << DENOMINATION_NAMES[i];
         total_cents -= (n_coins * DENOMINATIONS[i]);
     }
     return total_cents;
