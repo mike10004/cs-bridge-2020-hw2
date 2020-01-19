44fd21eadfa3780a1a6ad21a773e33436f88217a
Mike Chaberski <mchaberski@gmail.com>
2020-01-19 17:10:18 -0500

    q2: remove loop and arrays

--- hw2q2/mac937_hw2_q2.cpp
+++ hw2q2/mac937_hw2_q2.cpp
@@ -2,29 +2,29 @@
 
 using namespace std;
 
-const int DENOMINATIONS[] = {25, 10, 5, 1};
-const char* DENOMINATION_NAMES[] = {"quarters", "dimes", "nickels", "pennies"};
-const int DENOMINATIONS_COUNT = 4;
+const int CENTS_PER_QUARTER = 25;
+const int CENTS_PER_DIME = 10;
+const int CENTS_PER_NICKEL = 5;
+const int CENTS_PER_PENNY = 1;
 
 int main() {
     cout << "Please enter your amount in the format of dollars and cents separated by a space:\n";
     int n_dollars, n_cents, total_cents;
     cin >> n_dollars >> n_cents;
-    cout << n_dollars << " dollars and " << n_cents << " cents are:\n";
+    
     total_cents = (n_dollars * 100) + n_cents;
-    for (int i = 0; i < DENOMINATIONS_COUNT; i++)
-    {
-        int n_coins = total_cents / DENOMINATIONS[i];
-        if (i == (DENOMINATIONS_COUNT - 1))
-        {
-            cout << " and ";
-        }
-        else if (i > 0)
-        {
-            cout << ", ";
-        }
-        cout << n_coins << " " << DENOMINATION_NAMES[i];
-        total_cents -= (n_coins * DENOMINATIONS[i]);
-    }
-    return total_cents;
+    int n_quarters, n_dimes, n_nickels, n_pennies;
+    n_quarters = total_cents / CENTS_PER_QUARTER;
+    total_cents = total_cents - (n_quarters * CENTS_PER_QUARTER);
+    n_dimes = total_cents / CENTS_PER_DIME;
+    total_cents = total_cents - (n_dimes * CENTS_PER_DIME);
+    n_nickels = total_cents / CENTS_PER_NICKEL;
+    n_pennies = total_cents - (n_nickels * CENTS_PER_NICKEL);
+    
+    cout << n_dollars << " dollars and " << n_cents << " cents are:\n";
+    cout << n_quarters << " quarters, " 
+         << n_dimes << " dimes, "
+         << n_nickels << " nickels and "
+         << n_pennies << " pennies\n";
+    return 0;
 }
