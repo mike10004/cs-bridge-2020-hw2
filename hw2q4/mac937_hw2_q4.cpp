47b0adcd6233e6c4a4255d860150fc31fbfd19b4
Mike Chaberski <mchaberski@gmail.com>
2020-01-19 17:17:09 -0500

    q4: remove helper functions

--- hw2q4/mac937_hw2_q4.cpp
+++ hw2q4/mac937_hw2_q4.cpp
@@ -2,34 +2,20 @@
 
 using namespace std;
 
-void show_result(int operand1, int operand2, const char* op, int result)
-{
-    cout << operand1 << " "
-              << op << " "
-              << operand2 << " = "
-              << result << endl;
-}
-
-void show_result(int operand1, int operand2, const char* op, double result)
-{
-    cout << operand1 << " "
-              << op << " "
-              << operand2 << " = "
-              << result << endl;
-}
-
 int main() {
     int operand1, operand2;
+    double operand1Double, operand2Double;
     cout << "Please enter two positive integers, separated by a space:\n";
     cin >> operand1 >> operand2;
+    operand1Double = operand1;
+    operand2Double = operand2;
     cout.setf(ios::fixed);
     cout.precision(1);
-    show_result(operand1, operand2, "+", operand1 + operand2);
-    show_result(operand1, operand2, "–", operand1 - operand2);
-    show_result(operand1, operand2, "*", operand1 * operand2);
-    double div_result = double(operand1) / double(operand2);
-    show_result(operand1, operand2, "/", div_result);
-    show_result(operand1, operand2, "div", operand1 / operand2);
-    show_result(operand1, operand2, "mod", operand1 % operand2);
+    cout << operand1 << " + " << operand2 << " = " << (operand1 + operand2) << endl;
+    cout << operand1 << " – " << operand2 << " = " << (operand1 - operand2) << endl;
+    cout << operand1 << " * " << operand2 << " = " << (operand1 * operand2) << endl;
+    cout << operand1 << " / " << operand2 << " = " << (operand1Double / operand2Double) << endl;
+    cout << operand1 << " div " << operand2 << " = " << (operand1 / operand2) << endl;
+    cout << operand1 << " mod " << operand2 << " = " << (operand1 % operand2) << endl;
     return 0;
 }
