72574c042d2e366b69e19ea47caa813ada102519
Mike Chaberski <mchaberski@gmail.com>
2020-01-19 13:12:44 -0500

    q3: use namespace std

--- hw2q3/mac937_hw2_q3.cpp
+++ hw2q3/mac937_hw2_q3.cpp
@@ -1,25 +1,27 @@
 #include <iostream>
 
+using namespace std;
+
 const int MINUTES_PER_HOUR = 60;
 const int HOURS_PER_DAY = 24;
 
 int main() {
     int john_minutes, john_hours, john_days;
     int bill_minutes, bill_hours, bill_days;
-    std::cout << "Please enter the number of days John has worked: ";
-    std::cin >> john_days;
-    std::cout << "Please enter the number of hours John has worked: ";
-    std::cin >> john_hours;
-    std::cout << "Please enter the number of minutes John has worked: ";
-    std::cin >> john_minutes;
-    std::cout << std::endl;
-    std::cout << "Please enter the number of days Bill has worked: ";
-    std::cin >> bill_days;
-    std::cout << "Please enter the number of hours Bill has worked: ";
-    std::cin >> bill_hours;
-    std::cout << "Please enter the number of minutes Bill has worked: ";
-    std::cin >> bill_minutes;
-    std::cout << std::endl;
+    cout << "Please enter the number of days John has worked: ";
+    cin >> john_days;
+    cout << "Please enter the number of hours John has worked: ";
+    cin >> john_hours;
+    cout << "Please enter the number of minutes John has worked: ";
+    cin >> john_minutes;
+    cout << endl;
+    cout << "Please enter the number of days Bill has worked: ";
+    cin >> bill_days;
+    cout << "Please enter the number of hours Bill has worked: ";
+    cin >> bill_hours;
+    cout << "Please enter the number of minutes Bill has worked: ";
+    cin >> bill_minutes;
+    cout << endl;
     int total_minutes = john_minutes + bill_minutes;
     int total_hours = john_hours + bill_hours;
     int total_days = john_days + bill_days;
@@ -27,7 +29,7 @@ int main() {
     total_minutes = total_minutes % MINUTES_PER_HOUR;
     total_days = total_days + (total_hours / HOURS_PER_DAY);
     total_hours = total_hours % HOURS_PER_DAY;
-    std::cout << "The total time both of them worked together is: "
+    cout << "The total time both of them worked together is: "
               << total_days << " days, "
               << total_hours << " hours and "
               << total_minutes << " minutes.\n";
